19a4acd2947217b5da9f56b524e821c2758154cf
Aleksandr Poliakov <master@artwist.ru>
2024-02-20 14:02:00 +0300

    removes static method from redis cache

--- src/db/cache/redis_storage.py
+++ src/db/cache/redis_storage.py
@@ -1,37 +1,22 @@
 import logging
-from functools import wraps
 
 from redis.asyncio import Redis
 
 from configs.settings import RedisCacheSettings
 from db.cache.cache_storage import CacheStorage
+from db.redis_core import RedisCore
 from utils.wrappers import backoff
 
 
-class RedisStorage(CacheStorage):
-    _redis: Redis | None = None
+class RedisStorage(CacheStorage, RedisCore):
     _settings = RedisCacheSettings()
 
-    @staticmethod
-    def initialize(func):
-        @wraps(func)
-        async def inner(self, *args, **kwargs):
-            if self._redis is None:
-                self._redis = await Redis(
-                    host=self._settings.host,
-                    port=self._settings.port,
-                    db=self._settings.db,
-                ).initialize()
-            return await func(self, *args, **kwargs)
-
-        return inner
-
-    @initialize
+    @RedisCore.initialize
     @backoff(max_attempts=3)
     async def get_cache(self, key: str) -> str | None:
         return await self._redis.get(key)
 
-    @initialize
+    @RedisCore.initialize
     @backoff(max_attempts=3)
     async def put_cache(self, key: str, value: str, expired: int = 0) -> None:
         if expired > 0:
@@ -42,7 +27,7 @@ class RedisStorage(CacheStorage):
             logging.error(f"expired value should be greater than 0, "
                           f"but {expired} was passed")
 
-    @initialize
+    @RedisCore.initialize
     @backoff(max_attempts=3)
     async def delete_cache(self, key: str) -> None:
         await self._redis.delete(key)
