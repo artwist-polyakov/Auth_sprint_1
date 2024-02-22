ee1a1d2012d0a77facfef3e2d0e72fd0fb4a0f66
Aleksandr Poliakov <master@artwist.ru>
2024-02-22 12:17:36 +0300

    обновляет настройки безопасности

--- src/configs/security_settings.py
+++ src/configs/security_settings.py
@@ -2,7 +2,8 @@ from pydantic_settings import BaseSettings
 
 
 class JWTSecuritySettings(BaseSettings):
-    jwt_access_token_expires: int = 3600
+    access_token_expire_minutes: int = 60*60
+    refresh_token_expire_minutes: int = 500*60
     # jwt_secret_key: str = 'secret'
     # jwt_token_location: set[str] = {'cookies'}
     jwt_cookie_csrf_protect: bool = False
