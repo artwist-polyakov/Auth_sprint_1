130baf25e96cc22d65321fe77b014768724c9211
Aleksandr Poliakov <master@artwist.ru>
2024-02-23 17:19:47 +0300

    добавляет условия в процессор логаута

--- src/middlewares/logout_processor.py
+++ src/middlewares/logout_processor.py
@@ -12,12 +12,13 @@ ACCESS_TOKEN_KEY = "access_token"
 class CheckLogoutMiddleware(BaseHTTPMiddleware):
     async def dispatch(self, request: Request, call_next):
         token = request.cookies.get(ACCESS_TOKEN_KEY)
-        if token:
+        resource = request.url.path[1:]
+        if token and not ('login' in resource or 'sign_up' in resource):
             token = AccessTokenContainer(
                 **dict_from_jwt(token)
             )
             if await get_creator().get_logout_storage().is_blacklisted(token):
-                raise HTTPException(status_code=401, detail="Token is logout")
+                raise HTTPException(status_code=401, detail="Token is logout, please re-login")
         try:
             response = await call_next(request)
             return response
