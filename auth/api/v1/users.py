3dd7adcf4b90e6403e1f8f4792273f81ad23cbec
Aleksandr Poliakov <master@artwist.ru>
2024-02-25 12:16:49 +0300

    создаёт ручку для запроса прав

--- auth/api/v1/users.py
+++ auth/api/v1/users.py
@@ -6,6 +6,7 @@ from api.v1.utils.api_convertor import APIConvertor
 from db.models.token_models.access_token_container import AccessTokenContainer
 from fastapi import APIRouter, Cookie, Depends
 from fastapi.responses import JSONResponse, Response
+from services.models.permissions import RBACInfo
 from services.user_service import UserService, get_user_service
 from utils.jwt_toolkit import dict_from_jwt, get_jwt_settings
 from utils.wrappers import value_error_handler
@@ -270,6 +271,37 @@ async def get_login_history(
         **dict_from_jwt(access_token)
     )
     response: dict = await service.get_login_history(token.user_id)
+    return JSONResponse(
+        status_code=200,
+        content=response
+    )
+
+
+@router.get(
+    path="/check_permissions",
+    summary="Check permissions",
+    description="Check permissions for current user"
+)
+async def check_permissions(
+        resource: str,
+        verb: str,
+        access_token: str = Cookie(None),
+        service: UserService = Depends(get_user_service)
+) -> Response:
+    if not access_token:
+        return JSONResponse(
+            status_code=401,
+            content='Invalid access token'
+        )
+    token = AccessTokenContainer(
+        **dict_from_jwt(access_token)
+    )
+    rbac = RBACInfo(
+        role=token.role,
+        resource=resource,
+        verb=verb
+    )
+    response: bool = await service.check_permissions(token, rbac)
     return JSONResponse(
         status_code=200,
         content=response
