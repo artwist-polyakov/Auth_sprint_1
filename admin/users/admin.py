1a194eeeab80f3815349b2820365525602f253ca
Diana Dumskaia <dianadumskaia@yandex.ru>
2024-03-04 13:21:15 +0300

    меняет способ регистрации админки

--- admin/users/admin.py
+++ admin/users/admin.py
@@ -5,6 +5,7 @@ from users.models.role import Role
 from users.models.user import User
 
 
+@admin.register(User)
 class UserAdmin(admin.ModelAdmin):
     list_display = ('uuid', 'email', 'first_name', 'last_name', 'is_superuser', 'role', 'is_verified', 'created_at')
     search_fields = ('email', 'first_name', 'last_name')
@@ -12,17 +13,14 @@ class UserAdmin(admin.ModelAdmin):
     ordering = ('-created_at',)
 
 
+@admin.register(Role)
 class RoleAdmin(admin.ModelAdmin):
     list_display = ('uuid', 'role', 'resource', 'verb')
     search_fields = ('role', 'resource', 'verb')
 
 
+@admin.register(RefreshToken)
 class RefreshTokenAdmin(admin.ModelAdmin):
     list_display = ('uuid', 'user', 'active_till', 'created_at', 'user_device_type')
     search_fields = ('uuid', 'user__email', 'user_device_type')
     list_filter = ('user_device_type',)
-
-
-admin.site.register(User, UserAdmin)
-admin.site.register(Role, RoleAdmin)
-admin.site.register(RefreshToken, RefreshTokenAdmin)
