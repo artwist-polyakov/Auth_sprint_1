eafdcbbb53e0c3be471387b8b10a5e26d70ca7b0
Diana Dumskaia <dianadumskaia@yandex.ru>
2024-03-04 12:27:02 +0300

    добавляет приложение movies

--- admin/config/settings.py
+++ admin/config/settings.py
@@ -18,7 +18,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'users.apps.UsersConfig'
+    'users.apps.UsersConfig',
+    'movies.apps.MoviesConfig',
 ]
 
 MIDDLEWARE = [
