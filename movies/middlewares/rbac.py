9cef4116c48df3270e29933d2c27c177bffe14eb
Aleksandr Poliakov <master@artwist.ru>
2024-03-05 13:10:13 +0300

    настраивает rbac midleware на изящную деградацию

--- movies/middlewares/rbac.py
+++ movies/middlewares/rbac.py
@@ -1,3 +1,4 @@
+import asyncio
 import logging
 from http import HTTPStatus
 
@@ -5,7 +6,7 @@ import aiohttp
 from fastapi import HTTPException, Request
 from fastapi.responses import ORJSONResponse
 from starlette.middleware.base import BaseHTTPMiddleware
-from utils.jwt_toolkit import dict_from_jwt
+from utils.jwt_toolkit import dict_from_jwt, get_jwt_settings
 
 ACCESS_TOKEN_KEY = "access_token"
 REFRESH_TOKEN_KEY = "refresh_token"
@@ -14,6 +15,10 @@ ROLE_KEY = "role"
 
 # todo настроить EXСLUDED_PATHS через окружение
 EXСLUDED_PATHS = ['docs', 'openapi.json', 'api/openapi', 'api/openapi.json']
+
+
+class AuthUnavailableException(Exception):
+    pass
 
 
 def translate_method_to_action(method: str) -> str:
@@ -26,13 +31,27 @@ def translate_method_to_action(method: str) -> str:
     return method_permission_mapping.get(method.upper(), 'read')
 
 
-async def get_response(url: str, params: dict = None, method: str = 'GET', cookies: dict = None):
+async def get_response(
+        url: str,
+        params: dict = None,
+        method: str = 'GET',
+        cookies: dict = None,
+        headers: dict = None
+):
     """
     Функция отправляет асинхронный запрос на сервер
     и возвращает ответ
     """
-    async with aiohttp.ClientSession(cookies=cookies if cookies else None) as session:
-        async with session.request(method=method.lower(), url=url, params=params) as response:
+    async with aiohttp.ClientSession(
+            cookies=cookies if cookies else None,
+            headers=headers
+    ) as session:
+        async with session.request(
+                method=method.lower(),
+                url=url,
+                params=params
+        ) as response:
+            logging.warning(f"response: {response}")
             body = await response.json()
             status = response.status
             return body, status
@@ -46,12 +65,28 @@ async def has_permission(user_role, resource_name, required_permission):
         'verb': required_permission
     }
     logging.warning(f"params: {params}")
-    result, status = await get_response(url=url, params=params)
+    try:
+        result, status = await get_response(url=url, params=params)
+    except (aiohttp.ClientError, asyncio.TimeoutError) as e:
+        raise AuthUnavailableException() from e
     logging.warning(f"result: {result}")
     logging.warning(f"status: {status}")
     if status == HTTPStatus.OK:
         return result.get('permission', False)
     return False
+
+
+async def get_degradeted_response():
+    headers = {
+        'X-Internal-Token': get_jwt_settings().internal_secret_token
+    }
+    url = 'http://movies:8000/api/v1/films/search/'
+    params = {'query': 'star', 'per_page': 10, 'page': 1}
+    result, status = await get_response(url=url, params=params, headers=headers)
+    logging.warning(f"result: {result}")
+    return ORJSONResponse(
+        status_code=HTTPStatus.SERVICE_UNAVAILABLE,
+        content=result)
 
 
 class RBACMiddleware(BaseHTTPMiddleware):
@@ -60,7 +95,9 @@ class RBACMiddleware(BaseHTTPMiddleware):
         action = translate_method_to_action(request_method)
         resource = request.url.path[1:]
         logging.warning(f"resource: {resource}")
-        if resource not in EXСLUDED_PATHS:
+        if request.headers.get('X-Internal-Token') == get_jwt_settings().internal_secret_token:
+            pass
+        elif resource not in EXСLUDED_PATHS:
             token = request.cookies.get(ACCESS_TOKEN_KEY)
             role = UNAUTHORIZED_ROLE
             is_superuser = False
@@ -78,10 +115,19 @@ class RBACMiddleware(BaseHTTPMiddleware):
                 raise HTTPException(
                     status_code=HTTPStatus.UNAUTHORIZED,
                     detail="Bad credentials")
-            if not is_superuser and not await has_permission(role, resource.split("/")[2], action):
-                return ORJSONResponse(
-                    status_code=HTTPStatus.FORBIDDEN,
-                    content="Permission denied")
+            try:
+                permitted = await has_permission(
+                    role,
+                    resource.split("/")[2],
+                    action
+                )
+                if not is_superuser and not permitted:
+                    return ORJSONResponse(
+                        status_code=HTTPStatus.FORBIDDEN,
+                        content="Permission denied")
+            except AuthUnavailableException:
+                return await get_degradeted_response()
+
         try:
             response = await call_next(request)
             return response
