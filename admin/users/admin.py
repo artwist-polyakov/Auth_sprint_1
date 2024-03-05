fe02dfd9f5ef57e6572ec1d5626b61e9505fa36f
Diana Dumskaia <dianadumskaia@yandex.ru>
2024-03-05 10:56:29 +0300

    убирает лишнее

--- admin/users/admin.py
+++ admin/users/admin.py
@@ -1,6 +1,4 @@
 from django.contrib import admin
-from users.models.refresh_token import RefreshToken
-from users.models.role import Role
 from users.models.user import User
 
 
@@ -12,15 +10,3 @@ class UserAdmin(admin.ModelAdmin):
     list_filter = ('is_superuser', 'role', 'is_verified')
     ordering = ('-created_at',)
 
-
-@admin.register(Role)
-class RoleAdmin(admin.ModelAdmin):
-    list_display = ('uuid', 'role', 'resource', 'verb')
-    search_fields = ('role', 'resource', 'verb')
-
-
-@admin.register(RefreshToken)
-class RefreshTokenAdmin(admin.ModelAdmin):
-    list_display = ('uuid', 'user', 'active_till', 'created_at', 'user_device_type')
-    search_fields = ('uuid', 'user__email', 'user_device_type')
-    list_filter = ('user_device_type',)
