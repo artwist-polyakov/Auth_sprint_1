36988f5d16c114f5a2b69f15932373792bf74cf1
Diana Dumskaia <dianadumskaia@yandex.ru>
2024-03-07 18:11:14 +0300

    movies работает вместе с jaeger

--- auth/entrypoint.sh
+++ auth/entrypoint.sh
@@ -8,16 +8,7 @@ wait_for_postgres() {
    echo "Postgres is ready!"
 }
 
-wait_for_jaeger() {
-   echo "Waiting for Jaeger..."
-   while ! nc -z jaeger "$JAEGER_PORT"; do
-     sleep 1
-   done
-   echo "Jaeger is ready!"
-}
-
 wait_for_postgres
-wait_for_jaeger
 
 export PYTHONPATH=$PYTHONPATH:/.
 # команда: alembic upgrade head
