851c8538e86b77d32252023b5e31a5ae359f9f1d
Aleksandr Poliakov <master@artwist.ru>
2024-03-07 22:04:12 +0300

    обновляет сеттингс чтобы работал с переменной окружения для трейсера

--- movies/configs/settings.py
+++ movies/configs/settings.py
@@ -1,4 +1,5 @@
 import logging
+from functools import lru_cache
 
 from pydantic_settings import BaseSettings
 
@@ -28,6 +29,7 @@ class Settings(BaseSettings):
 
     jaeger_host: str = ...
     jaeger_port: int = ...
+    jaeger_logs_in_console: bool = False
 
     def get_logging_level(self) -> int:
         return log_levels.get(self.logging_level, logging.INFO)
@@ -78,3 +80,8 @@ class JWTSecuritySettings(BaseSettings):
 
     class Config:
         env_file = '.env'
+
+
+@lru_cache
+def get_settings():
+    return settings
