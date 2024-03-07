16b3ea5e86e8f07b1e1a8fea07c78d3f3b2fb697
Diana Dumskaia <dianadumskaia@yandex.ru>
2024-03-07 13:58:10 +0300

    указан правильный порт для jaeger

--- movies/main.py
+++ movies/main.py
@@ -54,7 +54,9 @@ async def before_request(request: Request, call_next):
     response = await call_next(request)
     request_id = request.headers.get('X-Request-Id')
     if not request_id:
-        return ORJSONResponse(status_code=status.HTTP_400_BAD_REQUEST, content={'detail': 'X-Request-Id is required'})
+        return ORJSONResponse(
+            status_code=status.HTTP_400_BAD_REQUEST,
+            content={'detail': 'X-Request-Id is required'})
     return response
 
 
