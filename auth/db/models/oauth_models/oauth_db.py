5d20c86b5abcdb70c7dea71a79927eb5f5530127
Aleksandr Poliakov <master@artwist.ru>
2024-03-08 22:00:36 +0300

    добавляет сущность для отправки на сохранение в базу

---
+++ auth/db/models/oauth_models/oauth_db.py
@@ -0,0 +1,9 @@
+from uuid import UUID
+
+from db.models.oauth_models.oauth_token import OAuthToken
+from db.models.oauth_models.user_model import OAuthUserModel
+
+
+class OAuthDBModel(OAuthToken, OAuthUserModel):
+    uuid: UUID
+    user_id: str
