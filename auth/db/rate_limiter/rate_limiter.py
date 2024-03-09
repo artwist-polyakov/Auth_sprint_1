d9f459ee27a8e00d7df96ef145bf8f55f077be21
Aleksandr Poliakov <master@artwist.ru>
2024-03-09 23:07:10 +0300

    объявляет все методы рейт лимитера как async

--- auth/db/rate_limiter/rate_limiter.py
+++ auth/db/rate_limiter/rate_limiter.py
@@ -4,13 +4,13 @@ from abc import ABC, abstractmethod
 class RateLimitter(ABC):
 
     @abstractmethod
-    def add_request(self, key: str, value: str, timeout: int) -> None:
+    async def add_request(self, key: str, value: str, timeout: int) -> None:
         pass
 
     @abstractmethod
-    def get_num_requests(self, key: str) -> int:
+    async def get_num_requests(self, key: str) -> int:
         pass
 
     @abstractmethod
-    def is_within_limit(self, key: str, max_requests) -> bool:
+    async def is_within_limit(self, key: str, max_requests) -> bool:
         pass
