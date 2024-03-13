a02064f3537158d0fca7f68879379a0ced333f12
Aleksandr Poliakov <master@artwist.ru>
2024-03-13 19:52:53 +0300

    выносит из базового класс авторизации метод в оауф наследника

--- auth/services/oauth_service.py
+++ auth/services/oauth_service.py
@@ -1,6 +1,9 @@
-from abc import ABC, abstractmethod
+import uuid
+from abc import ABC
+from abc import abstractmethod
 
 from db.auth.user import User
+from db.models.oauth_models.oauth_db import OAuthDBModel
 from db.models.oauth_models.oauth_token import OAuthToken
 from db.models.oauth_models.user_model import OAuthUserModel
 from db.models.token_models.access_token_container import AccessTokenContainer
@@ -37,3 +40,27 @@ class OAUTHService(BaseAuthService, ABC):
             await self._save_user_to_oauth(user_info, tokens, str(user.uuid))
             return await self._add_refresh_token(user, user_device_type)
         return None
+
+    async def _save_user_to_oauth(
+            self,
+            user_info: OAuthUserModel,
+            tokens: OAuthToken,
+            user_id: str
+    ) -> bool:
+        exists = await self._postgres.get_yandex_oauth_user(user_info.email)
+        if exists:
+            return False
+        model = OAuthDBModel(
+            uuid=str(uuid.uuid4()),
+            email=user_info.email,
+            first_name=user_info.first_name,
+            last_name=user_info.last_name,
+            access_token=tokens.access_token,
+            refresh_token=tokens.refresh_token,
+            oauth_source=self._oauth_method,
+            token_type=tokens.token_type,
+            expires_in=tokens.expires_in,
+            user_id=user_id
+        )
+        await self._postgres.add_single_data(model, 'oauth')
+        return True
