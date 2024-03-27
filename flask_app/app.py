cc54d8f174946927706ee41d8aa37602d8fc4669
Diana Dumskaia <dianadumskaia@yandex.ru>
2024-03-27 12:39:39 +0300

    создает шаблоны ручек

--- flask_app/app.py
+++ flask_app/app.py
@@ -1,12 +1,33 @@
-from flask import Flask
-
+from flask import Flask, request, jsonify
 
 app = Flask(__name__)
 
 
-@app.route('/hello-world')
-def hello_world():
-    return 'Hello, World!'
+@app.route('/view_event', methods=['POST'])
+async def view_event():
+    data = request.get_json()
+    user_uuid = data.get('user_uuid')
+    film_uuid = data.get('film_uuid')
+
+    return jsonify({'message': 'View event recorded'})
+
+
+@app.route('/player_event', methods=['POST'])
+async def player_event():
+    data = request.get_json()
+    user_uuid = data.get('user_uuid')
+    film_uuid = data.get('film_uuid')
+    event_type = data.get('event_type')
+    timestamp = data.get('timestamp')
+
+    return jsonify({'message': 'Player event recorded'})
+
+
+@app.route('/custom_event', methods=['POST'])
+async def custom_event():
+    data = request.get_json()
+
+    return jsonify({'message': 'Custom event recorded'})
 
 
 if __name__ == '__main__':
