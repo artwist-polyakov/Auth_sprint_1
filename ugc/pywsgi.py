15b76144c8551df3d06dc6f2da9226c30ef79483
Diana Dumskaia <dianadumskaia@yandex.ru>
2024-03-27 13:15:25 +0300

    переносит flask_app в  /ugc/

--- ugc/pywsgi.py
+++ ugc/pywsgi.py
@@ -1,4 +1,4 @@
-from configs.settings import settings
+from core.settings import settings
 from gevent import monkey
 
 monkey.patch_all()
@@ -6,5 +6,5 @@ monkey.patch_all()
 from app import app  # noqa
 from gevent.pywsgi import WSGIServer  # noqa
 
-http_server = WSGIServer(('', settings.flask_port), app)
+http_server = WSGIServer(('', settings.flask.port), app)
 http_server.serve_forever()
