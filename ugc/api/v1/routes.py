b1ab17493d9346dfe0880d06cafb764d83c43327
Aleksandr Poliakov <master@artwist.ru>
2024-03-30 14:26:23 +0300

    выводит результаты записи сообщения

--- ugc/api/v1/routes.py
+++ ugc/api/v1/routes.py
@@ -1,8 +1,11 @@
+from http import HTTPStatus
+from flask import jsonify, Response
 from api.v1.models.custom_event import CustomEvent
 from api.v1.models.player_event import PlayerEvent
 from api.v1.models.view_event import ViewEvent
 from app import app, events
-
+from services.queue_service import get_queue_service
+import time
 API_PREFIX = '/ugc/v1'
 
 
@@ -26,15 +29,30 @@ API_PREFIX = '/ugc/v1'
 #     """
 
 @app.post(f'{API_PREFIX}/view_event', summary="Record a view event", tags=[events])
-async def view_event(query: ViewEvent):
-    return {"status": "ok"}
+def view_event(query: ViewEvent) -> tuple[Response, int]:
+    start_time = time.time()
+    status, result = get_queue_service().process_event(query)
+    if status == HTTPStatus.OK:
+        return jsonify({"status": f"ok, speed = {time.time()-start_time} s"}), HTTPStatus.OK
+    else:
+        return jsonify({"status": "error", "details": result}), status
 
 
 @app.post(f'{API_PREFIX}/player_event', summary="Record a player event", tags=[events])
-async def player_event(query: PlayerEvent):
-    return {"status": "ok"}
+def player_event(query: PlayerEvent) -> tuple[Response, int]:
+    start_time = time.time()
+    status, result = get_queue_service().process_event(query)
+    if status == HTTPStatus.OK:
+        return jsonify({"status": f"ok, speed = {time.time()-start_time} s"}), HTTPStatus.OK
+    else:
+        return jsonify({"status": "error", "details": result}), status
 
 
 @app.post(f'{API_PREFIX}/custom_event', summary="Record a custom event", tags=[events])
-async def custom_event(query: CustomEvent):
-    return {"status": "ok"}
+def custom_event(query: CustomEvent) -> tuple[Response, int]:
+    start_time = time.time()
+    status, result = get_queue_service().process_event(query)
+    if status == HTTPStatus.OK:
+        return jsonify({"status": f"ok, speed = {time.time()-start_time} s"}), HTTPStatus.OK
+    else:
+        return jsonify({"status": "error", "details": result}), status
