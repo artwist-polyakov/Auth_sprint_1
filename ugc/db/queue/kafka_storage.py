2a11e8c95bf5fd3755a351912540912586ba9605
Aleksandr Poliakov <master@artwist.ru>
2024-03-30 22:19:19 +0300

    Revert "выносит кафка продюсер в переменную"
    This reverts commit 75beca037b4934b994182f3f96e9c0644e1d1550.

--- ugc/db/queue/kafka_storage.py
+++ ugc/db/queue/kafka_storage.py
@@ -1,6 +1,6 @@
 import logging
 import traceback
-from functools import lru_cache, wraps
+from functools import lru_cache
 
 from core.settings import settings
 from db.queue.message_broker_storage import (MessageBrokerConsumer,
@@ -12,36 +12,27 @@ from kafka import KafkaConsumer, KafkaProducer
 class KafkaCore:
     _connection = f'{settings.kafka.host}:{settings.kafka.port}'
 
-    def __init__(self):
-        self._producer = KafkaProducer(bootstrap_servers=[self._connection])
-
-    @staticmethod
-    def refresh_producer(func):
-        @wraps(func)
-        async def inner(self, *args, **kwargs):
-            try:
-                return func(self, *args, **kwargs)
-            except Exception as e:
-                logging.warning(f'error: {e}, {traceback.format_exc()}')
-                self._producer = KafkaProducer(bootstrap_servers=[self._connection])
-                return func(self, *args, **kwargs)
-
-        return inner
-
     async def close(self):
         ...
 
 
 class KafkaRepository(KafkaCore, MessageBrokerProducer, MessageBrokerConsumer):
 
-    @KafkaCore.refresh_producer
     def produce(self, data: KafkaModel):
         try:
-            return self._producer.send(
+            logging.warning(f'before produce int kafka: {self._connection}')
+            producer = KafkaProducer(bootstrap_servers=[self._connection])
+            logging.warning('producer created')
+            future = producer.send(
                 topic=data.topic,
                 key=data.key.encode('UTF-8'),
                 value=data.value.encode('UTF-8')
             )
+            # Ожидаем, пока сообщение будет отправлено
+            result = future.get(timeout=10)
+            logging.warning(f'after produce: {result}')
+            # producer.close()
+            return result
         except Exception as e:
             logging.warning(f'error: {e}, {traceback.format_exc()}')
             raise e
