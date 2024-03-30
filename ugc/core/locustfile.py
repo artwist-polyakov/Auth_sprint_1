dc95592297238744c81c9b5cb400926c7fbccc79
samtonck <samtonck@gmail.com>
2024-03-30 23:42:50 +0300

    178 | locust

---
+++ ugc/core/locustfile.py
@@ -0,0 +1,18 @@
+from locust import HttpUser, task, between
+
+
+class UGCTest(HttpUser):
+    wait_time = between(1, 5)
+
+    @task(1)
+    def custom_event(self):
+        self.client.get("http://localhost:5555/ugc/v1/custom_event?user_uuid=1234&event_type=1234&timestamp=1234")
+
+    @task(2)
+    def player_event(self):
+        self.client.get("http://localhost:5555/ugc/v1/player_event?user_uuid=1234&film_uuid=1234&event_type=seek&event_value=1234&timestamp=1234")
+
+    @task(3)
+    def view_event(self):
+        self.client.get("http://localhost:5555/ugc/v1/view_event?user_uuid=1234&film_uuid=1234")
+
