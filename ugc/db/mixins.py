b1ef62dbbe68d14c7df7f2b4996ea80a6f34a639
Кира Кузнецова <kirakuznetsova@MacBook-Pro-Kira.local>
2024-03-30 15:53:02 +0400

    148 | Добавление моделей

--- ugc/db/mixins.py
+++ ugc/db/mixins.py
@@ -1,5 +1,6 @@
 from uuid import UUID, uuid4
 
+from sqlalchemy_utils import Timestamp
 from sqlmodel import Field
 
 
@@ -7,3 +8,7 @@ class UUIDMixin:
     """Класс-миксин UUID id первичный ключ."""
 
     id: UUID = Field(primary_key=True, default_factory=uuid4, nullable=False)
+
+
+class TimeStampWithUUID(Timestamp, UUIDMixin):
+    ...
--- ugc/db/models.py
+++ ugc/db/models.py
@@ -1,6 +1,6 @@
 from uuid import UUID
 
-from db.mixins import UUIDMixin
+from db.mixins import TimeStampWithUUID
 from sqlmodel import Field, SQLModel
 
 
@@ -8,5 +8,19 @@ class UgcSqlModel(SQLModel):
     ...
 
 
-class Click(UgcSqlModel, UUIDMixin):
+class Click(UgcSqlModel, TimeStampWithUUID):
     user_id: UUID = Field(nullable=False)
+    movie_id: UUID = Field(nullable=False)
+
+
+class PlayerEvent(UgcSqlModel, TimeStampWithUUID):
+    user_id: UUID = Field(nullable=False)
+    movie_id: UUID = Field(nullable=False)
+    type: str = Field(nullable=False)
+    depth: int = Field(nullable=False)
+
+
+class OtherEvent(UgcSqlModel, TimeStampWithUUID):
+    user_id: UUID = Field(nullable=False)
+    movie_id: UUID = Field(nullable=False)
+    type: str = Field(nullable=False)
