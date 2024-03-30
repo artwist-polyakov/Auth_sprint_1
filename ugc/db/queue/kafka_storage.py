32b2e2d23f64ff6c43c8b52411cc51d669ee1938
Aleksandr Poliakov <master@artwist.ru>
2024-03-30 22:21:54 +0300

    удаляет логи и оборачивает в статический метод вызов продюсера

--- ugc/db/queue/kafka_storage.py
+++ ugc/db/queue/kafka_storage.py
@@ -1,6 +1,6 @@
 import logging
 import traceback
-from functools import lru_cache
+from functools import lru_cache, wraps
 
 from core.settings import settings
 from db.queue.message_broker_storage import (MessageBrokerConsumer,
@@ -34,23 +34,15 @@ class KafkaCore:
 
 class KafkaRepository(KafkaCore, MessageBrokerProducer, MessageBrokerConsumer):
 
+    @KafkaCore.refresh_producer
     def produce(self, data: KafkaModel):
         try:
-            logging.warning(f'before produce int kafka: {self._connection}')
-            producer = KafkaProducer(bootstrap_servers=[self._connection])
-            logging.warning('producer created')
-            future = producer.send(
+            return self._producer.send(
                 topic=data.topic,
                 key=data.key.encode('UTF-8'),
                 value=data.value.encode('UTF-8')
             )
-            # Ожидаем, пока сообщение будет отправлено
-            result = future.get(timeout=10)
-            logging.warning(f'after produce: {result}')
-            # producer.close()
-            return result
         except Exception as e:
-            logging.warning(f'error: {e}, {traceback.format_exc()}')
             raise e
 
     async def consume(self, data):
