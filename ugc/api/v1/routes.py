1d0964992a5cb797c8acf709acc5bb37cf09ef1e
samtonck <samtonck@gmail.com>
2024-03-31 13:06:16 +0300

    178 | pulsar

--- ugc/api/v1/routes.py
+++ ugc/api/v1/routes.py
@@ -10,21 +10,13 @@ from services.queue_service import get_queue_service
 
 API_PREFIX = '/ugc/v1'
 
-
+# """
 # curl -X POST http://localhost:5555/ugc/v1/view_event \
 #  -H "Content-Type: application/json" \
 #  -d '{
-#    "events": [
-#      {
 #        "user_uuid": "user1",
 #        "film_uuid": "film1"
-#      },
-#      {
-#        "user_uuid": "user2",
-#        "film_uuid": "film2"
-#      }
-#    ]
-#  }'
+#      }'
 # {"status":"ok"}
 #     :param query:
 #     :return:
