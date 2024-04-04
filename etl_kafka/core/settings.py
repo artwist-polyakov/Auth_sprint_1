e6877db133cd12e61cdf0a3bd53c0683a49a56ec
Aleksandr Poliakov <master@artwist.ru>
2024-04-04 22:01:46 +0300

    добавляет настройки логина и пароля

--- etl_kafka/core/settings.py
+++ etl_kafka/core/settings.py
@@ -32,8 +32,10 @@ class ClickHouseSettings(_BaseSettings):
 
     model_config = SettingsConfigDict(env_prefix="clickhouse_")
     host: str
-    http_port: int
+    port: int
     database: str
+    username: str
+    password: str
 
 
 class Settings(CommonSettings):
