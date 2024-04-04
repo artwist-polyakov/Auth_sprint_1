acb40a22ad2ddd309e45f4730f84134eff1e99d0
Aleksandr Poliakov <master@artwist.ru>
2024-04-04 23:05:39 +0300

    добавляет генерацию текущего времени в моделях данных

--- event_generator/models/custom_event.py
+++ event_generator/models/custom_event.py
@@ -1,7 +1,9 @@
+import time
+
 from pydantic import BaseModel
 
 
 class CustomEvent(BaseModel):
     user_uuid: str
     event_type: str
-    timestamp: int
+    timestamp: int = time.monotonic_ns()
