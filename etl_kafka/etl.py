ac6446df9713492c8916790400c4ec77d0874c8e
Aleksandr Poliakov <master@artwist.ru>
2024-04-04 22:02:06 +0300

    запускает etl

--- etl_kafka/etl.py
+++ etl_kafka/etl.py
@@ -4,70 +4,67 @@ import json
 import asyncio
 
 from core.settings import settings
+from db.kafka_storage import get_kafka
 
 
 class ETL:
     _kafka_bootstrap_servers = [f'{settings.kafka.host}:{settings.kafka.port}']
 
+
     def __init__(self):
-        self.topics = ['player_events', 'view_events', 'custom_events']
+        self._topics = ['player_events', 'view_events', 'custom_events']
 
-    async def get_consumer(self):
-        consumer_conf = {
-            'bootstrap_servers': self._kafka_bootstrap_servers,
-            'group_id': 'etl_group',
-            'auto_offset_reset': 'earliest'
-        }
-        consumer = KafkaConsumer(*self.topics, **consumer_conf)
-        return consumer
+    async def load(self, clickhouse, data, type):
+        # query = (f"INSERT INTO {settings.clickhouse.database}.events "
+        #          f"(event_id, event_type, event_data, event_timestamp) "
+        #          f"VALUES (?, ?, ?, NOW())")
+        # params = (event_data['event_id'], event_type, json.dumps(event_data))
+        # client.insert(query, params)
 
-    async def transform(self, msg):
-        if msg is None or msg.error():
-            return None, None
+        match (type):
+            case 'player_events':
+                t = [data['user_uuid'], data['film_uuid'], data['event_type'], data['event_value'], data['timestamp']]
+                clickhouse.insert(
+                    f"{settings.clickhouse.database}.player_event",
+                    t,
+                    ['user_id', 'movie_id', 'type', 'event_value', 'timestamp']
+                )
+            case 'view_events':
+                t = [data['user_uuid'], data['film_uuid'], data['timestamp']]
+                clickhouse.insert(
+                    f"{settings.clickhouse.database}.click",
+                    t,
+                    ['user_id', 'movie_id', 'timestamp']
+                )
+            case 'custom_events':
+                t = [data['user_uuid'], data['event_type'], data['timestamp']]
+                clickhouse.insert(
+                    f"{settings.clickhouse.database}.other_event",
+                    t,
+                    ['user_id', 'type', 'timestamp']
+                )
 
-        value = msg.value().decode('utf-8')
-        event_data = json.loads(value)
 
-        topic = msg.topic()
-        if topic == 'player_events':
-            event_type = 'PlayerEvent'
-        elif topic == 'view_events':
-            event_type = 'ViewEvent'
-        elif topic == 'custom_events':
-            event_type = 'CustomEvent'
-        else:
-            raise ValueError('Unknown event type')
 
-        return event_data, event_type
-
-    async def load(self, client, event_data, event_type):
-        query = (f"INSERT INTO {settings.clickhouse.database}.events "
-                 f"(event_id, event_type, event_data, event_timestamp) "
-                 f"VALUES (?, ?, ?, NOW())")
-        params = (event_data['event_id'], event_type, json.dumps(event_data))
-        client.insert(query, params)
 
     async def run(self, clickhouse):
-        consumer = await self.get_consumer()
+        for msg in await get_kafka().consume():
+            data = msg.value.decode('utf-8')
+            topic = msg.topic
+            if data is not None:
+                await self.load(clickhouse, data, topic)
 
-        try:
-            for msg in consumer:
-                event_data, event_type = await self.transform(msg)
-                if event_data is None:
-                    continue
 
-                await self.load(clickhouse, event_data, event_type)
-
-        finally:
-            consumer.close()
 
 
 if __name__ == "__main__":
     etl = ETL()
     with clickhouse_connect.get_client(
-        host=settings.clickhouse.host,
-        port=settings.clickhouse.http_port,
-        database=settings.clickhouse.database,
+            host=settings.clickhouse.host,
+            port=settings.clickhouse.port,
+            database=settings.clickhouse.database,
+            usename=settings.clickhouse.username,
+            password=settings.clickhouse.password,
     ) as clickhouse:
         while True:
             asyncio.run(etl.run(clickhouse))
