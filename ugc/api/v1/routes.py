94796543051baac8a71855c620b54de6049a945a
Aleksandr Poliakov <master@artwist.ru>
2024-04-05 22:44:57 +0300

    DRY

--- ugc/api/v1/routes.py
+++ ugc/api/v1/routes.py
@@ -1,3 +1,4 @@
+import logging
 import time
 from http import HTTPStatus
 
@@ -15,6 +16,7 @@ event_blueprint = APIBlueprint(
     "/events", __name__, url_prefix=API_PREFIX, abp_tags=[events], doc_ui=True
 )
 
+
 # """
 # curl -X POST http://localhost:5555/ugc/v1/view_event \
 #  -H "Content-Type: application/json" \
@@ -27,94 +29,80 @@ event_blueprint = APIBlueprint(
 #     :return:
 #     """
 
+class InvalideTokenError(Exception):
+    pass
 
-@event_blueprint.post("/view_event", summary="Record a view event")
-def view_event(query: ViewEvent) -> tuple[Response, int]:
-    start_time = time.monotonic()
 
-    access_token_cookie = request.cookies.get('access_token')
+class NoTokenError(Exception):
+    pass
+
+
+def _get_token_from_cookie(request_container) -> str:
+    access_token_cookie = request_container.cookies.get('access_token')
     if not access_token_cookie:
-        return jsonify({"error": "Access token not found"}), HTTPStatus.UNAUTHORIZED
-
+        raise NoTokenError("Access token not found")
     try:
         decoded_token = jwt.decode(
             access_token_cookie,
             settings.token.openssl_key,
             algorithms=[settings.token.algorithm]
         )
-    except jwt.ExpiredSignatureError:
-        return jsonify({"error": "Token has expired"}), HTTPStatus.UNAUTHORIZED
-    except jwt.InvalidTokenError:
-        return jsonify({"error": "Invalid token"}), HTTPStatus.UNAUTHORIZED
+        return decoded_token['user_id']
+    except Exception as e:
+        raise InvalideTokenError("Invalid token")
 
-    user_id = decoded_token['user_id']
-    event_instance = ViewEvent(
-        user_uuid=user_id,
-        film_uuid=query.film_uuid,
-        timestamp=query.timestamp)
-    status, result = get_queue_service().process_event(event_instance)
+
+@event_blueprint.post("/view_event", summary="Record a view event")
+def view_event(query: ViewEvent) -> tuple[Response, int]:
+    start_time = time.monotonic()
+    if not query.user_uuid:
+        print("User uuid not found")
+        try:
+            query.user_uuid = _get_token_from_cookie(request)
+        except NoTokenError:
+            return jsonify({"error": "Access token not found"}), HTTPStatus.UNAUTHORIZED
+        except InvalideTokenError:
+            return jsonify({"error": "Invalid token"}), HTTPStatus.UNAUTHORIZED
+    status, result = get_queue_service().process_event(query)
+    print(f"Status: {status}, result: {result}")
+
     if status == HTTPStatus.OK:
-        return jsonify({"status": f"ok, speed = {time.monotonic()-start_time} s"}), HTTPStatus.OK
+        return jsonify({"status": f"ok, speed = {time.monotonic() - start_time} s"}), HTTPStatus.OK
     return jsonify({"status": "error", "details": result}), status
 
 
 @event_blueprint.post("/player_event", summary="Record a player event")
 def player_event(query: PlayerEvent) -> tuple[Response, int]:
     start_time = time.monotonic()
-
-    access_token_cookie = request.cookies.get('access_token')
-    if not access_token_cookie:
-        return jsonify({"error": "Access token not found"}), HTTPStatus.UNAUTHORIZED
-
-    try:
-        decoded_token = jwt.decode(
-            access_token_cookie,
-            settings.token.openssl_key,
-            algorithms=[settings.token.algorithm]
-        )
-    except jwt.ExpiredSignatureError:
-        return jsonify({"error": "Token has expired"}), HTTPStatus.UNAUTHORIZED
-    except jwt.InvalidTokenError:
-        return jsonify({"error": "Invalid token"}), HTTPStatus.UNAUTHORIZED
-
-    user_id = decoded_token['user_id']
-    event_instance = PlayerEvent(
-        user_uuid=user_id,
-        film_uuid=query.film_uuid,
-        event_type=query.event_type,
-        event_value=query.event_value,
-        timestamp=query.timestamp)
-    status, result = get_queue_service().process_event(event_instance)
+    if not query.user_uuid:
+        print("User uuid not found")
+        try:
+            query.user_uuid = _get_token_from_cookie(request)
+        except NoTokenError:
+            return jsonify({"error": "Access token not found"}), HTTPStatus.UNAUTHORIZED
+        except InvalideTokenError:
+            return jsonify({"error": "Invalid token"}), HTTPStatus.UNAUTHORIZED
+    status, result = get_queue_service().process_event(query)
+    print(f"Status: {status}, result: {result}")
     if status == HTTPStatus.OK:
-        return jsonify({"status": f"ok, speed = {time.monotonic()-start_time} s"}), HTTPStatus.OK
+        return jsonify({"status": f"ok, speed = {time.monotonic() - start_time} s"}), HTTPStatus.OK
     return jsonify({"status": "error", "details": result}), status
 
 
 @event_blueprint.post("/custom_event", summary="Record a custom event")
 def custom_event(query: CustomEvent) -> tuple[Response, int]:
     start_time = time.monotonic()
+    if not query.user_uuid:
+        print("User uuid not found")
+        try:
+            query.user_uuid = _get_token_from_cookie(request)
+        except NoTokenError:
+            return jsonify({"error": "Access token not found"}), HTTPStatus.UNAUTHORIZED
+        except InvalideTokenError:
+            return jsonify({"error": "Invalid token"}), HTTPStatus.UNAUTHORIZED
+    status, result = get_queue_service().process_event(query)
+    print(f"Status: {status}, result: {result}")
 
-    access_token_cookie = request.cookies.get('access_token')
-    if not access_token_cookie:
-        return jsonify({"error": "Access token not found"}), HTTPStatus.UNAUTHORIZED
-
-    try:
-        decoded_token = jwt.decode(
-            access_token_cookie,
-            settings.token.openssl_key,
-            algorithms=[settings.token.algorithm]
-        )
-    except jwt.ExpiredSignatureError:
-        return jsonify({"error": "Token has expired"}), HTTPStatus.UNAUTHORIZED
-    except jwt.InvalidTokenError:
-        return jsonify({"error": "Invalid token"}), HTTPStatus.UNAUTHORIZED
-
-    user_id = decoded_token['user_id']
-    event_instance = CustomEvent(
-        user_uuid=user_id,
-        event_type=query.event_type,
-        timestamp=query.timestamp)
-    status, result = get_queue_service().process_event(event_instance)
     if status == HTTPStatus.OK:
-        return jsonify({"status": f"ok, speed = {time.monotonic()-start_time} s"}), HTTPStatus.OK
+        return jsonify({"status": f"ok, speed = {time.monotonic() - start_time} s"}), HTTPStatus.OK
     return jsonify({"status": "error", "details": result}), status
