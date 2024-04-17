0c15fecfd4a5e4965c8080c968a69eb22b377f27
samtonck <samtonck@gmail.com>
2024-04-17 23:07:03 +0300

    214 part 1 | пайдантик модели

--- ugc/db/models.py
+++ ugc/db/models.py
@@ -24,3 +24,24 @@ class OtherEvent(UgcSqlModel, TimeStampWithUUID):
     user_id: UUID = Field(nullable=False)
     movie_id: UUID = Field(nullable=False)
     type: str = Field(nullable=False)
+
+
+class Critique(UgcSqlModel, TimeStampWithUUID):
+    rating: str = Field(nullable=False)
+    content: str = Field(nullable=False)
+
+
+class UserCritique(UgcSqlModel, TimeStampWithUUID):
+    user_id: UUID = Field(nullable=False)
+    critique_id: UUID = Field(nullable=False)
+
+
+class Film(UgcSqlModel, TimeStampWithUUID):
+    film_id: UUID = Field(nullable=False)
+    critique_ids: list[UUID] = Field(nullable=False)
+    film_rating: float = Field(nullable=False)
+
+
+class UserBookmarks(UgcSqlModel, TimeStampWithUUID):
+    user_id: UUID = Field(nullable=False)
+    film_ids: list[UUID] = Field(nullable=False)
