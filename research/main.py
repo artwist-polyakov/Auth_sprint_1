c733c0ce9a5df5bd48ef7c4a37ea2ab6dba2e06a
samtonck <samtonck@gmail.com>
2024-04-17 12:57:32 +0300

    204 | тест mongo в несколько итераций

--- research/main.py
+++ research/main.py
@@ -2,18 +2,28 @@ from uuid import uuid4
 
 # from research.elastic_tests import load_data
 from research.mongo_tests import insert_document, find_data
-
+ITERATIONS = 10
 N = 10000
-# Загрузка данных
-data = [{"id": str(uuid4()), "value": f"Example text {i}"} for i in range(N)]
-
-# # Вызов функции для загрузки данных
-# load_data(data)
 
 
 if __name__ == '__main__':
-    result_ids, time = insert_document(data=data)
-    print(f'Запись {N} строк произведена в МОНГО: \nВремя в сек.: {time}')  #  \ndata: {result_ids}
+    time_mongo_load = 0
+    for i in range(ITERATIONS):
+        data = [{"id": str(uuid4()), "value": f"Example text {i}"} for i in range(N)]
+        load_data_mongo, time = insert_document(data=data)
+        time_mongo_load += time
+    print(f'Среднее время записи данных ({ITERATIONS} - итераций) в МОНГО в сак.: {time_mongo_load / ITERATIONS}')
 
-    found_users, time = find_data(condition={}, multiple=True)
-    print(f'Чтение {N} строк произведена из МОНГО: \nВремя в сек: {time}')  #  \ndata: {found_users}
+    time_mongo_found = 0
+    for i in range(ITERATIONS):
+        found_data_mongo, time = find_data(condition={}, multiple=True)
+        time_mongo_found += time
+    print(f'Среднее время чтения данных ({ITERATIONS} - итераций) из МОНГО в сак.: {time_mongo_load / ITERATIONS}')
+
+
+    # time = load_data(data)
+    # print(f'Запись {N} строк произведена в Эластик: \nВремя в сек.: {time}')
+
+    # read_data, time = find_data(condition={}, multiple=True)
+    # print(f'Чтение {N} строк произведена из МОНГО: \nВремя в сек: {time}')  #  \ndata: {found_users}
+
