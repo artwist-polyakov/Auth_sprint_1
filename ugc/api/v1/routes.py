0775e5dca7b873d3f3c0134728079e8f27dbf2bb
Кира Кузнецова <kirakuznetsova@MacBook-Pro-Kira.local>
2024-04-18 14:52:30 +0300

    215 | Add handlers for bookmarks and critiques

--- ugc/api/v1/routes.py
+++ ugc/api/v1/routes.py
@@ -1,18 +1,26 @@
 import time
 from http import HTTPStatus
+from uuid import UUID
 
 import jwt
 from api.v1.models.custom_event import CustomEvent
 from api.v1.models.player_event import PlayerEvent
 from api.v1.models.view_event import ViewEvent
-from app import API_PREFIX, events
+from app import API_PREFIX, events, films
 from core.settings import settings
 from flask import Response, jsonify, request
 from flask_openapi3 import APIBlueprint
 from services.queue_service import get_queue_service
 
+from .models.bookmark import Bookmark
+from .models.critique import Critique
+
 event_blueprint = APIBlueprint(
     "/events", __name__, url_prefix=API_PREFIX, abp_tags=[events], doc_ui=True
+)
+
+films_blueprint = APIBlueprint(
+    "/films", __name__, url_prefix=API_PREFIX, abp_tags=[films], doc_ui=True
 )
 
 
@@ -25,16 +33,16 @@ class NoTokenError(Exception):
 
 
 def _get_token_from_cookie(request_container) -> str:
-    access_token_cookie = request_container.cookies.get('access_token')
+    access_token_cookie = request_container.cookies.get("access_token")
     if not access_token_cookie:
         raise NoTokenError("Access token not found")
     try:
         decoded_token = jwt.decode(
             access_token_cookie,
             settings.token.openssl_key,
-            algorithms=[settings.token.algorithm]
+            algorithms=[settings.token.algorithm],
         )
-        return decoded_token['user_id']
+        return decoded_token["user_id"]
     except Exception:
         raise InvalidTokenError("Invalid token")
 
@@ -52,7 +60,10 @@ def view_event(query: ViewEvent) -> tuple[Response, int]:
             return jsonify({"error": "Invalid token"}), HTTPStatus.UNAUTHORIZED
     status, result = get_queue_service().process_event(query)
     if status == HTTPStatus.OK:
-        return jsonify({"status": f"ok, speed = {time.monotonic() - start_time} s"}), HTTPStatus.OK
+        return (
+            jsonify({"status": f"ok, speed = {time.monotonic() - start_time} s"}),
+            HTTPStatus.OK,
+        )
     return jsonify({"status": "error", "details": result}), status
 
 
@@ -69,7 +80,10 @@ def player_event(query: PlayerEvent) -> tuple[Response, int]:
             return jsonify({"error": "Invalid token"}), HTTPStatus.UNAUTHORIZED
     status, result = get_queue_service().process_event(query)
     if status == HTTPStatus.OK:
-        return jsonify({"status": f"ok, speed = {time.monotonic() - start_time} s"}), HTTPStatus.OK
+        return (
+            jsonify({"status": f"ok, speed = {time.monotonic() - start_time} s"}),
+            HTTPStatus.OK,
+        )
     return jsonify({"status": "error", "details": result}), status
 
 
@@ -86,5 +100,121 @@ def custom_event(query: CustomEvent) -> tuple[Response, int]:
             return jsonify({"error": "Invalid token"}), HTTPStatus.UNAUTHORIZED
     status, result = get_queue_service().process_event(query)
     if status == HTTPStatus.OK:
-        return jsonify({"status": f"ok, speed = {time.monotonic() - start_time} s"}), HTTPStatus.OK
+        return (
+            jsonify({"status": f"ok, speed = {time.monotonic() - start_time} s"}),
+            HTTPStatus.OK,
+        )
     return jsonify({"status": "error", "details": result}), status
+
+
+@films_blueprint.get(
+    "/rated_films", summary="Get a list of films that the user has rated"
+)
+def get_rated_films(user_id: UUID) -> tuple[Response, int]:
+    # 1/0 # for sentry test
+    if not user_id:
+        try:
+            user_id = _get_token_from_cookie(request)
+        except NoTokenError:
+            return jsonify({"error": "Access token not found"}), HTTPStatus.UNAUTHORIZED
+        except InvalidTokenError:
+            return jsonify({"error": "Invalid token"}), HTTPStatus.UNAUTHORIZED
+
+    try:
+        # Обработка user_id
+        return jsonify({"details": "all good"}), 200
+
+    except Exception as e:
+        return jsonify({"status": "error", "details": str(e)}), 400
+
+
+@films_blueprint.get(
+    "/film_rating_by_id", summary="Get a list of films that the user has rated"
+)
+def get_film_rating_by_id(film_id: UUID) -> tuple[Response, int]:
+    # 1/0 # for sentry test
+    try:
+        user_id = _get_token_from_cookie(request)
+    except NoTokenError:
+        return jsonify({"error": "Access token not found"}), HTTPStatus.UNAUTHORIZED
+    except InvalidTokenError:
+        return jsonify({"error": "Invalid token"}), HTTPStatus.UNAUTHORIZED
+
+    try:
+        # Обработка
+        return jsonify({"details": "all good"}), 200
+
+    except Exception as e:
+        return jsonify({"status": "error", "details": str(e)}), 400
+
+
+@films_blueprint.post("/rating", summary="Add bookmark")
+def add_rating(critique: Critique) -> tuple[Response, int]:
+    # 1/0 # for sentry test
+    try:
+        user_id = _get_token_from_cookie(request)
+    except NoTokenError:
+        return jsonify({"error": "Access token not found"}), HTTPStatus.UNAUTHORIZED
+    except InvalidTokenError:
+        return jsonify({"error": "Invalid token"}), HTTPStatus.UNAUTHORIZED
+
+    try:
+        # Обработка
+        return jsonify({"details": "all good"}), 200
+
+    except Exception as e:
+        return jsonify({"status": "error", "details": str(e)}), 400
+
+
+@films_blueprint.delete("/rating", summary="Delete rating")
+def delete_rating(critique_id: UUID) -> tuple[Response, int]:
+    # 1/0 # for sentry test
+    try:
+        user_id = _get_token_from_cookie(request)
+    except NoTokenError:
+        return jsonify({"error": "Access token not found"}), HTTPStatus.UNAUTHORIZED
+    except InvalidTokenError:
+        return jsonify({"error": "Invalid token"}), HTTPStatus.UNAUTHORIZED
+
+    try:
+        # Обработка
+        return jsonify({"details": "all good"}), 200
+
+    except Exception as e:
+        return jsonify({"status": "error", "details": str(e)}), 400
+
+
+@films_blueprint.post("/bookmark", summary="Add bookmark")
+def add_bookmark(critique: Bookmark) -> tuple[Response, int]:
+    # 1/0 # for sentry test
+    try:
+        user_id = _get_token_from_cookie(request)
+    except NoTokenError:
+        return jsonify({"error": "Access token not found"}), HTTPStatus.UNAUTHORIZED
+    except InvalidTokenError:
+        return jsonify({"error": "Invalid token"}), HTTPStatus.UNAUTHORIZED
+
+    try:
+        # Обработка
+        return jsonify({"details": "all good"}), 200
+
+    except Exception as e:
+        return jsonify({"status": "error", "details": str(e)}), 400
+
+
+@films_blueprint.delete("/bookmark", summary="Delete bookmark")
+def delete_bookmark(bookmark_id: UUID) -> tuple[Response, int]:
+    # 1/0 # for sentry test
+    try:
+        user_id = _get_token_from_cookie(request)
+    except NoTokenError:
+        return jsonify({"error": "Access token not found"}), HTTPStatus.UNAUTHORIZED
+    except InvalidTokenError:
+        return jsonify({"error": "Invalid token"}), HTTPStatus.UNAUTHORIZED
+
+    try:
+        # Обработка
+        return jsonify({"details": "all good"}), 200
+
+    except Exception as e:
+        return jsonify({"status": "error", "details": str(e)}), 400
--- ugc/app.py
+++ ugc/app.py
@@ -11,6 +11,7 @@ API_PREFIX = "/ugc/v1"
 
 info = Info(title="UGC Service API", version="1.0.0")
 events = Tag(name="events", description="Event tracking system")
+films = Tag(name="films", description="Film UGC data")
 server = Server(url="http://localhost:5555", description="Local development server")
 
 sentry_sdk.init(
@@ -23,28 +24,35 @@ app = OpenAPI(__name__, info=info, doc_prefix="/ugc/openapi", servers=[server])
 
 # app = Flask(__name__)
 
+
 @app.before_request
 def start_request():
     g.request_id = request.headers.get("X-Request-Id", str(uuid.uuid4()))
-    logging.info(f"Request started. "
-                 f"Method: {request.method}, "
-                 f"URL: {request.url}, "
-                 f"X-Request-Id: {g.request_id}")
+    logging.info(
+        f"Request started. "
+        f"Method: {request.method}, "
+        f"URL: {request.url}, "
+        f"X-Request-Id: {g.request_id}"
+    )
 
 
 @app.after_request
 def end_request(response):
     response.headers["X-Request-Id"] = g.request_id
-    logging.info(f"Request finished. "
-                 f"Method: {request.method}, "
-                 f"URL: {request.url}, "
-                 f"X-Request-Id: {g.request_id}")
+    logging.info(
+        f"Request finished. "
+        f"Method: {request.method}, "
+        f"URL: {request.url}, "
+        f"X-Request-Id: {g.request_id}"
+    )
     return response
 
 
 from api.v1.routes import event_blueprint  # noqa: F401, E402
+from api.v1.routes import films_blueprint  # noqa: F401, E402
 
 app.register_api(event_blueprint)
+app.register_api(films_blueprint)
 
 CORS(app)
 
