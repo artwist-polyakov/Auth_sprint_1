0525baddc4129dcc2d5e20decd15da71ec8dc530
Aleksandr Poliakov <master@artwist.ru>
2024-04-30 00:14:13 +0300

    инжектит таск парамы

--- notifications/api/v1/notifications.py
+++ notifications/api/v1/notifications.py
@@ -1,5 +1,7 @@
-from fastapi import APIRouter
 from http import HTTPStatus
+
+from api.v1.models.tasks_params import TasksParams
+from fastapi import APIRouter, Depends, Query
 
 router = APIRouter()
 
@@ -10,5 +12,8 @@ router = APIRouter()
     summary="Send Notification",
     description="Send a notification to users"
 )
-async def send_notification() -> str:
+async def send_notification(
+        users: list[str] = Query([], alias="users"),
+        params: TasksParams = Depends()
+) -> str:
     return "Notification sent successfully"
