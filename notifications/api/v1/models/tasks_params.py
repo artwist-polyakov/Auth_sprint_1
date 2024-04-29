0c02ca5f973d50be669c322f3ac1e2a36bb72479
Aleksandr Poliakov <master@artwist.ru>
2024-04-30 00:13:26 +0300

    добавляет класс для параметров инпута в уведомлениях

---
+++ notifications/api/v1/models/tasks_params.py
@@ -0,0 +1,25 @@
+from enum import Enum
+
+from pydantic import BaseModel, Field
+
+
+class MessageType(str, Enum):
+    EMAIL = "email"
+    SMS = "sms"
+    PUSH = "push"
+
+
+class TasksParams(BaseModel):
+    title: str = Field(
+        ...,
+        description="Notification title"
+    )
+    message: str = Field(
+        ...,
+        description="Notification message",
+
+    )
+    type: MessageType = Field(
+        MessageType.EMAIL,
+        description="Notification type"
+    )
