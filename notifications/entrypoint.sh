e138ec5246eeb8118544ab6dda528d81640105ed
Aleksandr Poliakov <master@artwist.ru>
2024-04-30 22:02:29 +0300

    добавляет скрипт ожидания постгрес

--- notifications/entrypoint.sh
+++ notifications/entrypoint.sh
@@ -1,3 +1,13 @@
 #!/bin/bash
 
+wait_for_postgres() {
+   echo "Waiting for Postgres..."
+   while ! nc -z "$NOTIFICATIONS_DB_HOST" "$NOTIFICATIONS_DB_PORT"; do
+     sleep 1
+   done
+   echo "Postgres is ready!"
+}
+
+wait_for_postgres
+
 uvicorn main:app --proxy-headers --host $NOTIFICATIONS_HOST --port $NOTIFICATIONS_PORT
