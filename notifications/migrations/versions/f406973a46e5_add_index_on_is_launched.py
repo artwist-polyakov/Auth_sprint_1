dd49adf2e3732733ddc389818e09766d867e94bd
Aleksandr Poliakov <master@artwist.ru>
2024-05-01 21:12:33 +0300

    flake8 isort

--- notifications/migrations/versions/f406973a46e5_add_index_on_is_launched.py
+++ notifications/migrations/versions/f406973a46e5_add_index_on_is_launched.py
@@ -9,7 +9,6 @@ from typing import Sequence, Union
 
 from alembic import op
 
-
 # revision identifiers, used by Alembic.
 revision: str = 'f406973a46e5'
 down_revision: Union[str, None] = '05933464dddb'
@@ -18,7 +17,13 @@ depends_on: Union[str, Sequence[str], None] = None
 
 
 def upgrade() -> None:
-    op.create_index('idx_is_launched', 'tasks', ['is_launched'], unique=False, schema='notifications')
+    op.create_index(
+        'idx_is_launched',
+        'tasks',
+        ['is_launched'],
+        unique=False,
+        schema='notifications'
+    )
 
 
 def downgrade() -> None:
