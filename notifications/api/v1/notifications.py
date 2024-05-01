4a0ce3ee4680273994acadf88b1c8bcd4d85fef5
Aleksandr Poliakov <master@artwist.ru>
2024-05-01 19:47:19 +0300

    подключает для апи сервис и конвертор

--- notifications/api/v1/notifications.py
+++ notifications/api/v1/notifications.py
@@ -1,9 +1,14 @@
+import logging
 from http import HTTPStatus
 
 from api.v1.models.task_result import TaskResult
 from api.v1.models.tasks_params import TasksParams
 from fastapi import APIRouter, Depends, Query
 from fastapi.responses import JSONResponse
+
+from api.v1.utils.convertors import TaskResponseConvertor
+from db.requests.task_request import TaskRequest, GetTaskInfo
+from service.tasks_service import TasksService, get_tasks_service
 
 router = APIRouter()
 
@@ -16,17 +21,29 @@ router = APIRouter()
 )
 async def create_task(
         users: list[str] = Query([], alias="users"),
-        params: TasksParams = Depends()
+        params: TasksParams = Depends(),
+        tasks_service: TasksService = Depends(get_tasks_service)
 ) -> TaskResult | JSONResponse:
     if not users:
         return JSONResponse(
             status_code=HTTPStatus.BAD_REQUEST,
             content={'message': 'Users are required'}
         )
-    return JSONResponse(
-        status_code=HTTPStatus.OK,
-        content={'message': f'params: {params.model_dump()}, users: {users}'}
+    task = TaskRequest(
+        title=params.title,
+        content=params.content,
+        user_ids=users,
+        type=params.type
     )
+    logging.warning(f"Task: {task}")
+    result = await tasks_service.handle_task_request(task)
+    logging.warning(f"Result: {result}")
+    if not result:
+        return JSONResponse(
+            status_code=HTTPStatus.BAD_REQUEST,
+            content={'message': 'Task creation failed'}
+        )
+    return TaskResponseConvertor.convert_to_response(result)
 
 
 @router.get(
@@ -36,9 +53,14 @@ async def create_task(
     description="Get information about the task by its id"
 )
 async def get_task_info(
-        task_id: int = Query(..., alias="task_id")
+        task_id: int = Query(..., alias="task_id"),
+        tasks_service: TasksService = Depends(get_tasks_service)
 ) -> TaskResult | JSONResponse:
-    return JSONResponse(
-        status_code=HTTPStatus.OK,
-        content={'message': f'Task info: {task_id}'}
-    )
+    request = GetTaskInfo(task_id=task_id)
+    result = await tasks_service.handle_task_request(request)
+    if not result:
+        return JSONResponse(
+            status_code=HTTPStatus.NOT_FOUND,
+            content={'message': 'Task not found'}
+        )
+    return TaskResponseConvertor.convert_to_response(result)
