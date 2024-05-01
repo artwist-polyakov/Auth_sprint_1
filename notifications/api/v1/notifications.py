4ec0e52ac165123e76f1906999d1e518f1fddb25
Aleksandr Poliakov <master@artwist.ru>
2024-05-01 16:02:34 +0300

    добавляет аннотации методов API возвращающие данные

--- notifications/api/v1/notifications.py
+++ notifications/api/v1/notifications.py
@@ -3,6 +3,7 @@ from http import HTTPStatus
 from fastapi import APIRouter, Depends, Query
 from fastapi.responses import JSONResponse
 
+from api.v1.models.task_result import TaskResult
 from api.v1.models.tasks_params import TasksParams
 
 router = APIRouter()
@@ -10,14 +11,14 @@ router = APIRouter()
 
 @router.post(
     path='/create',
-    response_model=str,
+    response_model=TaskResult,
     summary="Send Notification",
     description="Send a notification to users"
 )
 async def create_task(
         users: list[str] = Query([], alias="users"),
         params: TasksParams = Depends()
-) -> JSONResponse:
+) -> TaskResult | JSONResponse:
     if not users:
         return JSONResponse(
             status_code=HTTPStatus.BAD_REQUEST,
@@ -26,4 +27,19 @@ async def create_task(
     return JSONResponse(
         status_code=HTTPStatus.OK,
         content={'message': f'params: {params.model_dump()}, users: {users}'}
+    )
+
+
+@router.get(
+    path='/info',
+    response_model=TaskResult,
+    summary="Get task information",
+    description="Get information about the task by its id"
+)
+async def get_task_info(
+        task_id: int = Query(..., alias="task_id")
+) -> TaskResult | JSONResponse:
+    return JSONResponse(
+        status_code=HTTPStatus.OK,
+        content={'message': f'Task info: {task_id}'}
     )
