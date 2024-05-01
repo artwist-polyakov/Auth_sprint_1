a8d04918217bc779640c29498fa2b8c759743ae0
Aleksandr Poliakov <master@artwist.ru>
2024-05-01 20:52:29 +0300

    добавляет число ошибок ответ сервера

--- notifications/db/responses/task_response.py
+++ notifications/db/responses/task_response.py
@@ -6,6 +6,7 @@ class TaskResponse(BaseResponse):
     title: str
     sended_messages: int
     total_messages: int
+    with_errors: int = 0
     type: str
     created_at: int
     is_launched: bool
