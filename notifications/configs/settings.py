a2497291954f5c26c866832a1590d672cfdf5a6b
Aleksandr Poliakov <master@artwist.ru>
2024-05-01 19:36:48 +0300

    добавляет настройки подключения к базе данных нотификаций

--- notifications/configs/settings.py
+++ notifications/configs/settings.py
@@ -27,6 +27,12 @@ class Settings(BaseSettings):
     sentry_dsn: str = ...
     sentry_enable_tracing: bool = True
 
+    notifications_db_host: str = ...
+    notifications_db_port: int = ...
+    notifications_db_name: str = ...
+    notifications_db_user: str = ...
+    notifications_db_password: str = ...
+
     def get_logging_level(self) -> int:
         return log_levels.get(self.logging_level, logging.INFO)
 
@@ -35,6 +41,22 @@ class Settings(BaseSettings):
 
 
 settings = Settings()
+
+
+class PostgresDSN(BaseSettings):
+    host: str = settings.notifications_db_host
+    port: int = settings.notifications_db_port
+    db: str = settings.notifications_db_name
+    user: str = settings.notifications_db_user
+    password: str = settings.notifications_db_password
+
+    def get_dsn(self) -> str:
+        return f"postgresql+asyncpg://{self.user}:{self.password}@{self.host}:{self.port}/{self.db}"
+
+
+@lru_cache
+def get_postgres_dsn():
+    return PostgresDSN().get_dsn()
 
 
 @lru_cache
