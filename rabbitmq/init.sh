714455ea78a4246d7991e2369bd6d938a350359e
Aleksandr Poliakov <master@artwist.ru>
2024-05-02 15:26:10 +0300

    создаёт ноды ребита и инициализацтор

---
+++ rabbitmq/init.sh
@@ -0,0 +1,33 @@
+#!/bin/sh
+
+export RABBIT_MQ_HOST=${RABBIT_MQ_HOST:-"localhost"}
+export RABBIT_MQ_PORT=${RABBIT_MQ_PORT:-5672}
+export RABBIT_MQ_TASKS_KEY=${RABBIT_MQ_TASKS_KEY:-"tasks"}
+export RABBIT_MQ_NOTIFICATIONS_KEY=${RABBIT_MQ_NOTIFICATIONS_KEY:-"notifications"}
+export RABBIT_MQ_ENRICHED_KEY=${RABBIT_MQ_ENRICHED_KEY:-"enriched"}
+export RABBIT_MQ_TO_SENDING_KEY=${RABBIT_MQ_TO_SENDING_KEY:-"to_sending"}
+
+#waiting for rabbitmq-server to start
+set -e
+until timeout 1 bash -c 'cat < /dev/null > /dev/tcp/${RABBIT_MQ_HOST}/${RABBIT_MQ_PORT}'; do
+  >&2 echo "RabbitMQ is unavailable - sleeping"
+  sleep 1
+done
+
+>&2 echo "RabbitMQ is up - executing command"
+
+#executing command
+
+rabbitmqadmin declare exchange name=my_exchange type=direct
+
+rabbitmqadmin declare queue name=TasksQueue durable=true
+rabbitmqadmin declare queue name=NotificationsQueue durable=true
+rabbitmqadmin declare queue name=EnrichedQueue durable=true
+rabbitmqadmin declare queue name=ToSendingQueue durable=true
+
+rabbitmqadmin declare binding source=my_exchange destination_type=queue destination=TasksQueue routing_key=${RABBIT_MQ_TASKS_KEY}
+rabbitmqadmin declare binding source=my_exchange destination_type=queue destination=NotificationsQueue routing_key=${RABBIT_MQ_NOTIFICATIONS_KEY}
+rabbitmqadmin declare binding source=my_exchange destination_type=queue destination=EnrichedQueue routing_key=${RABBIT_MQ_ENRICHED_KEY}
+rabbitmqadmin declare binding source=my_exchange destination_type=queue destination=ToSendingQueue routing_key=${RABBIT_MQ_TO_SENDING_KEY}
+
+echo "Initialization completed"
