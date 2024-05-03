a7e56c3c96014cbe8176bdf769a231f7ff4c85fb
Aleksandr Poliakov <master@artwist.ru>
2024-05-03 18:56:11 +0300

    добавляет вывод etl

--- etl-notifications-tasks/etl.py
+++ etl-notifications-tasks/etl.py
@@ -1,7 +1,15 @@
 import logging
 
+from db.storage.postgres_storage import PostgresStorage
+
 logger = logging.getLogger('etl-tasks-logger')
 logger.setLevel(logging.INFO)
 logger.addHandler(logging.StreamHandler())
 
-logger.info('Ia yest cron')
+storage = PostgresStorage()
+tasks = storage.getNewTasks()
+
+logger.info("ETL started")
+
+for task in tasks:
+    logger.info(f"Task {task.id} is launched: {task.is_launched}")
