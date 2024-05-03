3d0546ead440e8ac7e59d74b7f233e1111c185d1
Aleksandr Poliakov <master@artwist.ru>
2024-05-03 22:57:56 +0300

    запускает процесс передачи  задач из базы в очередь

--- etl-notifications-tasks/etl.py
+++ etl-notifications-tasks/etl.py
@@ -1,4 +1,5 @@
 import logging
+from queue.rabbit_queue import RabbitQueue
 
 from db.storage.postgres_storage import PostgresStorage
 
@@ -7,9 +8,14 @@ logger.setLevel(logging.INFO)
 logger.addHandler(logging.StreamHandler())
 
 storage = PostgresStorage()
+rabbitmq = RabbitQueue()
+
 tasks = storage.getNewTasks()
 
 logger.info("ETL started")
 
 for task in tasks:
-    logger.info(f"Task {task.id} is launched: {task.is_launched}")
+    logger.info(f"Processing task {task.id}")
+    rabbitmq.push(task)
+    storage.markTaskLaunched(task.id)
+    logger.info(f"Task {task.id} processed")
