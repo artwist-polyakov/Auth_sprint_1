e7593b1cf6bb42282f791e5cb9e6f8dc0ff882a2
Aleksandr Poliakov <master@artwist.ru>
2024-05-04 22:31:52 +0300

    оптимизирует отправку задачи

--- notifications-workers/creation_worker/creation_worker.py
+++ notifications-workers/creation_worker/creation_worker.py
@@ -18,6 +18,7 @@ rabbitmq_tasks = RabbitQueue(get_settings().rabbit.tasks_queue)
 rabbitmq_notifications = RabbitQueue(get_settings().get_rabbit_settings().notifications_key)
 
 
+
 def handle_exit(sig, frame):
     print(f"{worker_id} received signal to terminate.")
     sys.exit(0)
@@ -25,16 +26,16 @@ def handle_exit(sig, frame):
 
 def handler(ch, method, properties, body):
     try:
-        data = Message(**json.loads(body.decode()))  # валидируем через пайдантик пришедшие данные
-        user_ids_list = deepcopy(data.user_ids)  # получаем список id
-        del data.user_ids  # удаляем поле из даты
-
-        for user_id in user_ids_list:
-            data_single_task = SingleTask(user_id=user_id, **data.__dict__)
-            rabbitmq_notifications.push(message=data_single_task)
+        data = Message(**ast.literal_eval(body.decode()))
+        task = SingleTask(**data.model_dump())
+        logger.info(f"Processing task {data}")
+        for user_id in data.user_ids:
+            task.user_id = user_id
+        rabbitmq_notifications.push(message=task)
+        ch.basic_ack(delivery_tag=method.delivery_tag)
     except Exception as e:
         print(f"Error in callback: {e}")
-        sys.stdout.flush()  # Принудительно записываем лог
+        sys.stdout.flush()
 
 
 signal.signal(signal.SIGTERM, handle_exit)
