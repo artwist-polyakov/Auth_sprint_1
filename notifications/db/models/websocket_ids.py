37d1ed932abfbeb825604e07cb578da9a1db438a
Aleksandr Poliakov <master@artwist.ru>
2024-05-04 23:11:46 +0300

    flake isort

--- notifications/db/models/websocket_ids.py
+++ notifications/db/models/websocket_ids.py
@@ -1,8 +1,7 @@
 from datetime import datetime
 
-from sqlalchemy import Boolean, Column, DateTime
-from sqlalchemy import Integer, String
-from sqlalchemy import Index
+from sqlalchemy import Boolean, Column, DateTime, Index, Integer, String
+
 from db.models.base import Base
 
 
