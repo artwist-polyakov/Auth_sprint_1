fdd1bd6185af8532c8941326a529d4285b7d8e04
Aleksandr Poliakov <master@artwist.ru>
2024-05-04 23:01:47 +0300

    flake isort

--- notifications/db/models/tasks.py
+++ notifications/db/models/tasks.py
@@ -1,18 +1,29 @@
 from datetime import datetime
 from enum import Enum
 
-from db.models.base import Base
 from sqlalchemy import Boolean, Column, DateTime
 from sqlalchemy import Enum as SQLEnum
 from sqlalchemy import Integer, String
 from sqlalchemy.dialects.postgresql import ARRAY
 from sqlalchemy.orm import relationship
 
+from db.models.base import Base
+
 
 class NotificationType(str, Enum):
     EMAIL = "email"
     SMS = "sms"
     PUSH = "push"
+
+
+class NotificationScenario(str, Enum):
+    DAILY = "daily"  # лайки, комменты, подписки
+    WEEKLY = "weekly"  # статистика
+    MONTHLY = "monthly"  # отчеты
+    WELCOME = "welcome"  # приветствие
+    BIRTHDAY = "birthday"  # поздравление с днем рождения
+    MAILINGS = "mailings"  # рассылки от админа
+    NEWS = "news"  # новые поступления каталога
 
 
 class Tasks(Base):
@@ -24,6 +35,11 @@ class Tasks(Base):
     content: str = Column(String, nullable=False)
     user_ids: list[str] = Column(ARRAY(String), nullable=False)
     type = Column(SQLEnum(NotificationType), nullable=False)
+    scenario = Column(
+        SQLEnum(NotificationScenario),
+        nullable=False,
+        default=NotificationScenario.DAILY
+    )
     created_at = Column(DateTime, default=datetime.utcnow)
     is_launched: bool = Column(Boolean, nullable=False, default=False)
     notifications = relationship(
