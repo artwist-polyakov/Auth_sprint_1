98315bf5490fc5a0766b1a79742c39dbc33ff5e1
samtonck <samtonck@gmail.com>
2024-05-04 23:27:49 +0300

    апдейт реббита

--- notifications-workers/enriching_worker/queue/rabbit_queue.py
+++ notifications-workers/enriching_worker/queue/rabbit_queue.py
@@ -1,19 +1,21 @@
 from queue.base_queue import BaseQueue
-from typing import Any, Callable
+from typing import Callable, TypeVar
 
 import pika
 from configs.settings import get_settings
-from models.task_result import TaskResult
+
+T = TypeVar('T')
 
 
 class RabbitQueue(BaseQueue):
-    def __init__(self):
+    def __init__(self, key: str):
         self.host = get_settings().rabbit.host
         self.port = get_settings().rabbit.amqp_port
         self.username = get_settings().rabbit.user
         self.password = get_settings().rabbit.password
         self.connection = None
         self.channel = None
+        self._key = key
 
     def __enter__(self):
         credentials = pika.PlainCredentials(self.username, self.password)
@@ -29,28 +31,35 @@ class RabbitQueue(BaseQueue):
     def __exit__(self, exc_type, exc_value, traceback):
         self.connection.close()
 
-    def push(self, task: TaskResult, queue, session=None) -> bool:
+    def push(self, message: T, session=None) -> bool:
         with self:
             self.channel.confirm_delivery()
             properties = pika.BasicProperties(
                 delivery_mode=2,
-                headers={"Task-Id": str(task.id)}
+                headers={"Task-Id": str(message.id)}
             )
-            print(task.model_dump())
             self.channel.basic_publish(
                 exchange=get_settings().get_rabbit_settings().exchange,
-                routing_key=queue,
-                body=str(task.model_dump()),
+                routing_key=self._key,
+                body=str(message.model_dump()),
                 properties=properties
             )
 
-    def pop(self, queue, handler: Callable[[Any, Any, Any, bytes], None]):
+    def pop(self,
+            handler: Callable[
+                [pika.channel.Channel,
+                 pika.spec.Basic.Deliver,
+                 pika.spec.BasicProperties,
+                 bytes],
+                None
+            ]):
         with self:
-            method_frame, header_frame, body = self.channel.basic_get(
-                queue=queue,
-                auto_ack=True,
-                on_message_callback=handler
+            self.channel.basic_consume(
+                queue=self._key,
+                on_message_callback=handler,
+                auto_ack=False
             )
+            self.channel.start_consuming()
 
     def close(self):
         pass
