3ab5d1312db806499ac7c0c04ef9918e0f05beea
Aleksandr Poliakov <master@artwist.ru>
2024-05-05 23:42:09 +0300

    удаляет отключившихся

--- ws/server.py
+++ ws/server.py
@@ -1,5 +1,8 @@
 import asyncio
 import websockets  # Установите этот пакет, если у вас его нет
+import logging
+
+logging.basicConfig(level=logging.INFO)
 
 peoples = {}  # Словарь будет содержать ник подключившегося человека и указатель на его websocket-соединение.
 
@@ -13,21 +16,27 @@ async def welcome(websocket: websockets.WebSocketServerProtocol) -> str:
 
 
 async def receiver(websocket: websockets.WebSocketServerProtocol, path: str) -> None:
-    name = await welcome(websocket)
-    while True:
-        # Получаем сообщение от абонента и решаем, что с ним делать
-        message = (await websocket.recv()).strip()
-        if message == '?':  # На знак вопроса вернём список ников подключившихся людей
-            await websocket.send(', '.join(peoples.keys()))
-            continue
-        else:  # Остальные сообщения попытаемся проанализировать и отправить нужному собеседнику
-            to, text = message.split(': ', 1)
-            if to in peoples:
-                # Пересылаем сообщение в канал получателя, указав отправителя
-                await peoples[to].send(f'Сообщение от {name}: {text}')
-                await websocket.send(f'ОК')
-            else:
-                await websocket.send(f'BAD')
+    name = None
+    try:
+        name = await welcome(websocket)
+        while True:
+            # Получаем сообщение от абонента и решаем, что с ним делать
+            message = (await websocket.recv()).strip()
+            if message == '?':  # На знак вопроса вернём список ников подключившихся людей
+                await websocket.send(', '.join(peoples.keys()))
+                continue
+            else:  # Остальные сообщения попытаемся проанализировать и отправить нужному собеседнику
+                to, text = message.split(': ', 1)
+                if to in peoples:
+                    # Пересылаем сообщение в канал получателя, указав отправителя
+                    await peoples[to].send(f'Сообщение от {name}: {text}')
+                    await websocket.send(f'ОК')
+                else:
+                    await websocket.send(f'BAD')
+    except websockets.exceptions.ConnectionClosed as e:
+        logging.info(f"Connection with {name.strip()} closed: {e}")
+        peoples.pop(name.strip(), None)
+        logging.info(f"{name.strip()} has been disconnected and removed from the list.")
 
 
 # Создаём сервер, который будет обрабатывать подключения
